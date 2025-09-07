7a40c18e91c972c095c2d41a8d0944bf28fb07ea
isujitkr <skr87981@gmail.com>
2025-09-07 12:13:49 +0530

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,10 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://lcrp-frontend.onrender.com"], 
+  credentials: true,
+}));
 app.use(express.json());
 
 
